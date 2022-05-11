5ffb3327c44a1530aa6cc748f41b26f7ce5f41ae
Osehi <osehiaseehilen@gmail.com>
2022-05-11 12:35:59 +0100

    getAniBlogUsecase created

---
+++ app/src/main/java/com/polishone/animalblog/common/domain/usecase/GetAniBlogsUseCase.kt
@@ -0,0 +1,21 @@
+package com.polishone.animalblog.common.domain.usecase
+
+import com.polishone.animalblog.common.constant.Resource
+import com.polishone.animalblog.common.domain.model.AniBlog
+import com.polishone.animalblog.common.domain.repository.AniBlogRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class GetAniBlogsUseCase @Inject constructor(private val getAniBlogRepository: AniBlogRepository) {
+
+    suspend operator fun invoke(): Flow<Resource<List<AniBlog>>> = flow {
+        emit(Resource.Loading(null))
+        try {
+            val response = getAniBlogRepository.getAniBlogs()
+            emit(Resource.Success(response))
+        } catch (e:Exception){
+            emit(Resource.Error("error occurred"))
+        }
+    }
+}
