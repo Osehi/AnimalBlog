850d1eb8197f6f212a3412b82481b14c2e52e243
Osehi <osehiaseehilen@gmail.com>
2022-05-14 12:03:00 +0100

    bind_function_created

--- app/src/main/java/com/polishone/animalblog/common/presentation/adapter/AniBlogAdapter.kt
+++ app/src/main/java/com/polishone/animalblog/common/presentation/adapter/AniBlogAdapter.kt
@@ -3,6 +3,7 @@ package com.polishone.animalblog.common.presentation.adapter
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.polishone.animalblog.common.domain.model.AniBlog
+import com.polishone.animalblog.common.utils.bindImage
 import com.polishone.animalblog.databinding.AniblogListItemBinding
 
 class AniBlogAdapter {
@@ -15,7 +16,15 @@ class AniBlogAdapter {
             val blogImage = binding.aniblogListItemAnimaliImage
             val blogDescription = binding.aniblogListItemDescriptionText
             // set the data to the view
-
+            val userImageUrl = aniBlog.owner.picture
+            bindImage(userImageUrl, userImage)
+            val blogImageUrl = aniBlog.image
+            bindImage(blogImageUrl, blogImage)
+            //  get the first and last name
+            val firstName = aniBlog.owner.firstName
+            val lastName = aniBlog.owner.lastName
+            username.text = "${firstName} ${lastName}"
+            blogDescription.text = aniBlog.text
         }
     }
 }
