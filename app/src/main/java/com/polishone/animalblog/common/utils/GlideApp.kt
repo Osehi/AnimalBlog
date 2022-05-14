78796a3b029f04355532e1c93211b1f83c8c78eb
Osehi <osehiaseehilen@gmail.com>
2022-05-14 11:46:22 +0100

    glide dependency and module added

---
+++ app/src/main/java/com/polishone/animalblog/common/utils/GlideApp.kt
@@ -0,0 +1,8 @@
+package com.polishone.animalblog.common.utils
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GlideApp : AppGlideModule() {
+}
