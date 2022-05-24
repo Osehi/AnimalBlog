90d56c542f6f6525628a73172fedca3fcf566f43
Osehi <osehiaseehilen@gmail.com>
2022-05-24 13:15:59 +0100

    type-converter-registered db

--- app/src/main/java/com/polishone/animalblog/common/data/cache/BlogDatabase.kt
+++ app/src/main/java/com/polishone/animalblog/common/data/cache/BlogDatabase.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.polishone.animalblog.common.constant.NetworkConstant
 import com.polishone.animalblog.common.domain.model.entity.AniBlogEntity
 
 @Database( entities = [AniBlogEntity::class, BlogKey::class], version = 1, exportSchema = false)
+@TypeConverters(RoomTypeConverter::class, ListOfStringToStringTypeConverter::class)
 abstract class BlogDatabase : RoomDatabase(){
 
     abstract fun getBlogDAO(): BlogDAO
